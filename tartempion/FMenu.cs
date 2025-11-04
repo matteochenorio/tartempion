6cdba32eb227f2826b7b06f8aa36d3b1dddd8871
kakpangni <krouericdonald@gmail.com>
2025-11-04 14:50:11 +0100

    BON DEBUT

--- tartempion/FMenu.cs
+++ tartempion/FMenu.cs
@@ -18,36 +18,10 @@ namespace tartempion
         {
             InitializeComponent();
         }
-        public static void ThreadProc()
-        {
-            Application.Run(new FConnexion());
-        }
 
         private void FMenu_Load(object sender, EventArgs e)
         {
             pbMenu.Image = Image.FromFile(@"U:\Romeuf\AP3 GSB 2025\Logo GSB\logo.png");
-        }
-
-        private void btnDeconnexion_Click(object sender, EventArgs e)
-        {
-            MonModelMission2.UtilisateurConnecte = null;
-            MonModelMission2.ConnexionValide = false;
-
-            System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadProc));
-            t.Start();
-            this.Close();
-        }
-
-        private void bsRole_CurrentChanged(object sender, EventArgs e)
-        {
-            //bsRole.DataSource = MonModelMission2.ListeSpecialite();
-            //Visiteur visiteurConnecte = (Visiteur)bsRole.Current;
-            //tbRole.Text = visiteurConnecte.Prenom.ToString();
-
-            bsRole.DataSource = MonModelMission2.ListeVisiteur();
-            //tbRole.Text = MonModelMission2.VisiteurConnecte.IdVisiteur.ToString();
-            //tbNom.Text = MonModelMission2.VisiteurConnecte.Nom.ToString();
-            //tbPrenom.Text = MonModelMission2.VisiteurConnecte.Prenom.ToString();
         }
     }
 }
--- tartempion/MonModelMission2.cs
+++ tartempion/MonModelMission2.cs
@@ -13,74 +13,14 @@ namespace tartempion
         private static TartempionContext monModel;
         private static Visiteur visiteurConnecte;
         private static bool connexionValide;
-        private static Visiteur utilisateurConnecte;
 
         public static TartempionContext MonModel { get => monModel; set => monModel = value; }
         public static Visiteur VisiteurConnecte { get => visiteurConnecte; set => visiteurConnecte = value; }
         public static bool ConnexionValide { get => connexionValide; set => connexionValide = value; }
-        public static Visiteur UtilisateurConnecte { get => utilisateurConnecte; set => utilisateurConnecte = value; }
 
         public static void init()
         {
             MonModel = new TartempionContext();
-        }
-
-        public static void ThreadProc()
-        {
-            Application.Run(new FMenu());
-        }
-
-        private static string GetMd5Hash(string PasswdSaisi)
-        {
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(PasswdSaisi);
-            byte[] hash = (MD5.Create()).ComputeHash(inputBytes);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                sb.Append(hash[i].ToString("x2"));
-            }
-            return sb.ToString();
-        }
-        
-        public static string validConnexion(string id, string mp)
-        {
-            ConnexionValide = false;
-            string message = "Id ou Mdp incorrect";
-            List<Visiteur> Listeu = monModel.Visiteurs.Where(x => x.Identifiant == id).ToList();
-            if (Listeu.Count == 1)
-            {
-                UtilisateurConnecte = Listeu[0];
-                if (UtilisateurConnecte.Password.Equals(GetMd5Hash(mp)))
-                {
-                    message = "connexion ok";
-                    ConnexionValide = true;
-                }
-            }
-            return message;
-        }
-
-        public static List<Visiteur> ListeVisiteur()
-        {
-            return MonModel.Visiteurs.ToList();
-        }
-
-        public static List<Medicament> ListeMedicament()
-        {
-            return MonModel.Medicaments.ToList();
-        }
-
-        public static List<Famille> ListeFamille()
-        {
-            return MonModel.Familles.ToList();
-        }
-
-        public static List<Medecin> ListeMedecin()
-        {
-            return MonModel.Medecins.ToList();
-        }
-        public static List<Specialite> ListeSpecialite()
-        {
-            return MonModel.Specialites.ToList();
         }
     }
 }
